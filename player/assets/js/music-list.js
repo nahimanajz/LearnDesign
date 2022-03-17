c2b4686dd1f7f12e1e62e57e8b757fb48fc9c998
nahimanajz <jazzonahimana@gmail.com>
2022-03-17 19:53:44 +0200

    feature: Player

---
+++ player/assets/js/music-list.js
@@ -0,0 +1,20 @@
+let allMusic = [
+    { 
+        name: 'Dinero by', 
+        artist: 'Jayc & Bulldog', 
+        img: 'player.png',
+        song: 'dinero',
+    },
+    { 
+        name: 'All Of The Lights', 
+        artist: 'Kanye West ft. Rihanna, Kid Cudi', 
+        img: 'guitar.jpg',
+        song: 'lights',
+    },
+    { 
+        name: '6 Foot 7 Foot', 
+        artist: 'Lil Wayne ft Cory Gunz', 
+        img: 'drink_water.jpg',
+        song: 'lilwayne',
+    }
+]
